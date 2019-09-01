b03458dafb88adcfdcc83e297132497d7ee0e81b
Bersabel Wondimagegnhu <bersabel.tesfa@gmail.com>
2019-09-01 19:24:23 -0400

    day4-question2

--- day4-lunch/day4-lunch-q2.py
+++ day4-lunch/day4-lunch-q2.py
@@ -1,62 +1,43 @@
 #!/usr/bin/env python3
 
 """
-Usage :.//00-scat
+./day4-lunch-q2.py ../results/stringtie/SRR072893/t_data.ctab ../results/stringtie/SRR072894/t_data.ctab
 """
 
 
-
 import sys
+import os
+import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
-import os
 
-name1 = sys.argv[1].split(os.sep)[-2]
-ctab1 = pd.read_csv(sys.argv[1], sep="\t" , index_col="t_name")
+name1 = sys.argv[1].split(os.sep)[-2] 
+ctab1 = pd.read_csv(sys.argv[1], sep="\t", index_col="t_name") 
 
-name2 = sys.argv[2].split(os.sep)[-2]
-ctab2 = pd.read_csv(sys.argv[2], sep="\t" , index_col="t_name")
+name2 = sys.argv[2].split(os.sep)[-2] 
+ctab2 = pd.read_csv(sys.argv[2], sep="\t", index_col="t_name")
 
-fpkm = { name1 : ctab1.loc[:,"FPKM"],
-         name2 : ctab2.loc[:,"FPKM"]}        
+fpkm = {name1: ctab1.loc[:,"FPKM"], 
+        name2: ctab2.loc[:,"FPKM"]}
 
-df = pd.DataFrame( fpkm )
+df = pd.DataFrame(fpkm) 
 
-goi = np.log2(df.loc[:,name1 ] +1)
-goi2 = np.log2(df.loc[:,name2 ] +1)
 
-print(df)
+fpkmss1 = np.log2(df.loc[:,name1] + 1) 
+fpkmss2 = np.log2(df.loc[:,name2] + 1)
 
-# exons = []
-# lengths = []
-# myexons = np.log2(exons)
-# mylengths = np.log2(lengths)
-#
-# for i, line in enumerate( open(sys.argv[1])):
-#     if i == 0:
-#         continue
-#     fields = line.rstrip("\n").split("\t")
-#     exons.append( int(fields[6]) )
-#     lengths.append ( int(fields[7]))
-#
-polyfit = np.polyfit(goi, goi2, 1) 
-poly1d = np.poly1d(polyfit)
-xv = np.linspace(0, 14, 100)
+m, b = np.polyfit(fpkmss1, fpkmss2, 1) 
+x = [fpkmss1.min(), fpkmss2.max()] 
+
 fig, ax = plt.subplots()
-ax.scatter( x= goi, y= goi2, alpha = 0.5, color="red")
-# ax.plot( [0,40], [0,20000], color="red")
-ax.set_title("scatterplot")
-ax.set_xlabel("log2 FPKMS {}".format(name1))
-ax.set_ylabel("log2 FPKMS {}".format(name2))
-
-ax.plot( xv,poly1d(xv) )
-fig.savefig("sample1-v-sample2.png")
-plt.close( fig )
+ax.scatter (fpkmss1, fpkmss2, s=3, alpha=0.2, color="red") 
+ax.plot(x, [(m*x1+b) for x1 in x], 
+    color="blue") 
+ax.set_title("Scatterplot of FPKMs")
+ax.set_xlabel("Log2 FPKM {}".format(name1))
+ax.set_ylabel("Log2 FPKM {}".format(name2))
 
 
+fig.savefig("ndscatterplot.png")
 
-#
-# my_data = np.log2( fpkms )
-# ax.hist( my_data, bins = 100 , density = True )
 
